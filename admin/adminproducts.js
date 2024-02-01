fed292395d9211286a92c0feb3e23b4f0b953a8f
Daniel <1175611@student.horizoncollege.nl>
2024-02-01 09:15:25 +0100

    322

--- admin/adminproducts.js
+++ admin/adminproducts.js
@@ -1,26 +1,26 @@
 let producten = JSON.parse(localStorage.getItem("producten"));
-//template van products in adminpanel
+// template van products in adminpanel
 
-function kaarten(producten) {
+function kaarten(producten2) {
     document.getElementById("productentemp").innerHTML = "";
-    producten.forEach((product, index) => {
+    producten2.forEach((product, index) => {
         const shoptemplate = document.getElementsByTagName("template")[0];
         const kaart = shoptemplate.content.cloneNode(true);
         kaart.querySelectorAll(".Id")[0].textContent = index;
         kaart.querySelectorAll(".Naam")[0].textContent = product.name;
         kaart.querySelectorAll(".Prijs")[0].textContent = `€${product.price}`;
         kaart.querySelectorAll(".Edit")[0].href = `edit.html?id=${index}`;
-       if(product.image) kaart.querySelectorAll("img")[0].src = product.image;
+        if (product.image) kaart.querySelectorAll("img")[0].src = product.image;
      document.getElementById("productentemp").appendChild(kaart);
 
      const verwijder = document.querySelectorAll(".remove")[index];
      verwijder.addEventListener("click", () => {
          if (index !== -1) {
-             producten.splice(producten.indexOf(product), 1);
+             producten2.splice(producten2.indexOf(product), 1);
 
              localStorage.setItem(
                  "producten",
-                 JSON.stringify(producten)
+                 JSON.stringify(producten2)
              );
              verwijder.closest("li").remove();
 
